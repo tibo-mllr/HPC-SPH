ca189e2892bc723be1cad8fd15e599ffec934f3d
Thibault Muller <thibault.muller19@gmail.com>
2024-02-20 12:40:36 +0100

    Second attempt

--- sph.py
+++ sph.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sph_cython
-from line_profiler import LineProfiler
 from scipy.special import gamma
 
 """
@@ -10,148 +9,6 @@ Philip Mocz (2020) Princeton Univeristy, @PMocz
 
 Simulate the structure of a star with SPH
 """
-
-
-def W(x, y, z, h):
-    """
-    Gaussian Smoothing kernel (3D)
-        x     is a vector/matrix of x positions
-        y     is a vector/matrix of y positions
-        z     is a vector/matrix of z positions
-        h     is the smoothing length
-        w     is the evaluated smoothing function
-    """
-
-    r = np.sqrt(x**2 + y**2 + z**2)
-
-    w = (1.0 / (h * np.sqrt(np.pi))) ** 3 * np.exp(-(r**2) / h**2)
-
-    return w
-
-
-def gradW(x, y, z, h):
-    """
-    Gradient of the Gaussian Smoothing kernel (3D)
-    x     is a vector/matrix of x positions
-    y     is a vector/matrix of y positions
-    z     is a vector/matrix of z positions
-    h     is the smoothing length
-    wx, wy, wz     is the evaluated gradient
-    """
-
-    r = np.sqrt(x**2 + y**2 + z**2)
-
-    n = -2 * np.exp(-(r**2) / h**2) / h**5 / (np.pi) ** (3 / 2)
-    wx = n * x
-    wy = n * y
-    wz = n * z
-
-    return wx, wy, wz
-
-
-def getPairwiseSeparations(ri, rj):
-    """
-    Get pairwise desprations between 2 sets of coordinates
-    ri    is an M x 3 matrix of positions
-    rj    is an N x 3 matrix of positions
-    dx, dy, dz   are M x N matrices of separations
-    """
-
-    M = ri.shape[0]
-    N = rj.shape[0]
-
-    # positions ri = (x,y,z)
-    rix = ri[:, 0].reshape((M, 1))
-    riy = ri[:, 1].reshape((M, 1))
-    riz = ri[:, 2].reshape((M, 1))
-
-    # other set of points positions rj = (x,y,z)
-    rjx = rj[:, 0].reshape((N, 1))
-    rjy = rj[:, 1].reshape((N, 1))
-    rjz = rj[:, 2].reshape((N, 1))
-
-    # matrices that store all pairwise particle separations: r_i - r_j
-    dx = rix - rjx.T
-    dy = riy - rjy.T
-    dz = riz - rjz.T
-
-    return dx, dy, dz
-
-
-def getDensity(r, pos, m, h):
-    """
-    Get Density at sampling loctions from SPH particle distribution
-    r     is an M x 3 matrix of sampling locations
-    pos   is an N x 3 matrix of SPH particle positions
-    m     is the particle mass
-    h     is the smoothing length
-    rho   is M x 1 vector of densities
-    """
-
-    M = r.shape[0]
-
-    dx, dy, dz = getPairwiseSeparations(r, pos)
-
-    rho = np.sum(m * W(dx, dy, dz, h), 1).reshape((M, 1))
-
-    return rho
-
-
-def getPressure(rho, k, n):
-    """
-    Equation of State
-    rho   vector of densities
-    k     equation of state constant
-    n     polytropic index
-    P     pressure
-    """
-
-    P = k * rho ** (1 + 1 / n)
-
-    return P
-
-
-def getAcc(pos, vel, m, h, k, n, lmbda, nu):
-    """
-    Calculate the acceleration on each SPH particle
-    pos   is an N x 3 matrix of positions
-    vel   is an N x 3 matrix of velocities
-    m     is the particle mass
-    h     is the smoothing length
-    k     equation of state constant
-    n     polytropic index
-    lmbda external force constant
-    nu    viscosity
-    a     is N x 3 matrix of accelerations
-    """
-
-    N = pos.shape[0]
-
-    # Calculate densities at the position of the particles
-    rho = getDensity(pos, pos, m, h)
-
-    # Get the pressures
-    P = getPressure(rho, k, n)
-
-    # Get pairwise distances and gradients
-    dx, dy, dz = getPairwiseSeparations(pos, pos)
-    dWx, dWy, dWz = sph_cython.gradW1(dx, dy, dz, h)
-
-    # Add Pressure contribution to accelerations
-    ax = -np.sum(m * (P / rho**2 + P.T / rho.T**2) * dWx, 1).reshape((N, 1))
-    ay = -np.sum(m * (P / rho**2 + P.T / rho.T**2) * dWy, 1).reshape((N, 1))
-    az = -np.sum(m * (P / rho**2 + P.T / rho.T**2) * dWz, 1).reshape((N, 1))
-
-    # pack together the acceleration components
-    a = np.hstack((ax, ay, az))
-
-    # Add external potential force
-    a -= lmbda * pos
-
-    # Add viscosity
-    a -= nu * vel
-
-    return a
 
 
 def main():
@@ -168,7 +25,6 @@ def main():
     k = 0.1  # equation of state constant
     n = 1  # polytropic index
     nu = 1  # damping
-    plotRealTime = True  # switch on for plotting as the simulation goes along
 
     # Generate Initial Conditions
     np.random.seed(42)  # set the random number generator seed
@@ -183,10 +39,6 @@ def main():
     )  # ~ 2.01
     m = M / N  # single particle mass
     pos = np.random.randn(N, 3)  # randomly selected positions and velocities
-    vel = np.zeros(pos.shape)
-
-    # calculate initial gravitational accelerations
-    acc = getAcc(pos, vel, m, h, k, n, lmbda, nu)
 
     # number of timesteps
     Nt = int(np.ceil(tEnd / dt))
@@ -201,49 +53,30 @@ def main():
     rr[:, 0] = rlin
     rho_analytic = lmbda / (4 * k) * (R**2 - rlin**2)
 
-    # Simulation Main Loop
-    for i in range(Nt):
-        # (1/2) kick
-        vel += acc * dt / 2
+    pos = sph_cython.main(N, Nt, dt, m, h, k, n, lmbda, nu)
 
-        # drift
-        pos += vel * dt
+    # get density for plotting
+    rho = np.asarray(sph_cython.getDensity(pos, pos, m, h))
 
-        # update accelerations
-        acc = getAcc(pos, vel, m, h, k, n, lmbda, nu)
+    plt.sca(ax1)
+    plt.cla()
+    cval = np.minimum((rho - 3) / 3, 1).flatten()
+    plt.scatter(pos[:, 0], pos[:, 1], c=cval, cmap=plt.cm.autumn, s=10, alpha=0.5)
+    ax1.set(xlim=(-1.4, 1.4), ylim=(-1.2, 1.2))
+    ax1.set_aspect("equal", "box")
+    ax1.set_xticks([-1, 0, 1])
+    ax1.set_yticks([-1, 0, 1])
+    ax1.set_facecolor("black")
+    ax1.set_facecolor((0.1, 0.1, 0.1))
 
-        # (1/2) kick
-        vel += acc * dt / 2
-
-        # update time
-        t += dt
-
-        # get density for plotting
-        rho = getDensity(pos, pos, m, h)
-
-        # plot in real time
-        if plotRealTime or (i == Nt - 1):
-            plt.sca(ax1)
-            plt.cla()
-            cval = np.minimum((rho - 3) / 3, 1).flatten()
-            plt.scatter(
-                pos[:, 0], pos[:, 1], c=cval, cmap=plt.cm.autumn, s=10, alpha=0.5
-            )
-            ax1.set(xlim=(-1.4, 1.4), ylim=(-1.2, 1.2))
-            ax1.set_aspect("equal", "box")
-            ax1.set_xticks([-1, 0, 1])
-            ax1.set_yticks([-1, 0, 1])
-            ax1.set_facecolor("black")
-            ax1.set_facecolor((0.1, 0.1, 0.1))
-
-            plt.sca(ax2)
-            plt.cla()
-            ax2.set(xlim=(0, 1), ylim=(0, 3))
-            ax2.set_aspect(0.1)
-            plt.plot(rlin, rho_analytic, color="gray", linewidth=2)
-            rho_radial = getDensity(rr, pos, m, h)
-            plt.plot(rlin, rho_radial, color="blue")
-            plt.pause(0.001)
+    plt.sca(ax2)
+    plt.cla()
+    ax2.set(xlim=(0, 1), ylim=(0, 3))
+    ax2.set_aspect(0.1)
+    plt.plot(rlin, rho_analytic, color="gray", linewidth=2)
+    rho_radial = np.asarray(sph_cython.getDensity(rr, pos, m, h))
+    plt.plot(rlin, rho_radial, color="blue")
+    plt.pause(0.001)
 
     # add labels/legend
     plt.sca(ax2)
