f142a7ff5c0568ebf61c3576eeb6fcf5ac8a45de
Thibault Muller <thibault.muller19@gmail.com>
2024-02-22 17:00:07 +0100

    Review

--- numba_code/main.py
+++ numba_code/main.py
@@ -14,7 +14,7 @@ Simulate the structure of a star with SPH
 
 def W(x, y, z, h):
     """
-    Gausssian Smoothing kernel (3D)
+    Gaussian Smoothing kernel (3D)
         x     is a vector/matrix of x positions
         y     is a vector/matrix of y positions
         z     is a vector/matrix of z positions
@@ -32,7 +32,7 @@ def W(x, y, z, h):
 @jit()
 def gradW(x, y, z, h):
     """
-    Gradient of the Gausssian Smoothing kernel (3D)
+    Gradient of the Gaussian Smoothing kernel (3D)
     x     is a vector/matrix of x positions
     y     is a vector/matrix of y positions
     z     is a vector/matrix of z positions
@@ -267,7 +267,7 @@ def main():
     plt.ylabel("density")
 
     # Save figure
-    plt.savefig("sph.png", dpi=240)
+    # plt.savefig("sph.png", dpi=240)
     plt.show()
 
     print(f"This is the average time: {sum(times[1:]) / len(times[1:])}")
